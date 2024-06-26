8e4791ca5ff05231fbb0ccd752e3b53f555a068a
Hamza Jamal Shahid Khan <161300035+Hamza-TE@users.noreply.github.com>
2024-06-26 12:18:08 +0530

    Initial commit

---
+++ constants/apiEndpoints.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+module.exports = {
+    API_ENDPOINTS: {
+        BACKEND_PORT: process.env.BACKEND_PORT,
+        GET_ASSISTANT_RESPONSE: process.env.GET_ASSISTANT_RESPONSE,
+        GET_ASTRA_DB_SEARCH: process.env.GET_ASTRA_DB_SEARCH
+    },
+    SUB_API_ENDPOINTS: {
+        CHATS: process.env.CHATS,
+        ASTRA_DB_SEARCH_TEXT: process.env.ASTRA_DB_SEARCH_TEXT,
+    }
+};
